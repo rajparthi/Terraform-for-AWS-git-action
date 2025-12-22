77b4e7156f856e4c8f80aab153d988b25356c538
rajparthi <rajparthi27@gmail.com>
2025-12-22 14:54:55 +0530

    change2

--- eks/backend.tf
+++ eks/backend.tf
@@ -10,7 +10,7 @@ terraform {
     bucket         = "aws-tf-pr001"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    use_lockfile = true
     encrypt        = true
   }
 }
